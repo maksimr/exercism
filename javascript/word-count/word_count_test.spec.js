bb046312a86c5f5ff9076d7acd78a5e395653555
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-09-07 18:52:11 +0300

    Add exporting of wordCount

--- javascript/word-count/word_count_test.spec.js
+++ javascript/word-count/word_count_test.spec.js
@@ -1,3 +1,5 @@
+var wordCount = require('./word_count');
+
 describe('word_count_test.spec', function() {
     it('should export word count function', function() {
         expect(wordCount).toBeDefined();
