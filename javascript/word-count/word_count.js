16e827d0ab3a3046a0273cacbbf9d1ad5a3ff54c
Evgenii Schepotiev <evgenii.schepotiev@jetbrains.com>
2015-09-07 18:58:12 +0300

    fix + test

--- javascript/word-count/word_count.js
+++ javascript/word-count/word_count.js
@@ -1,3 +1,6 @@
 module.exports = function wordCount() {
+    if(arguments.length===0) {
+        throw "Empty args";
+    }
 
 }
--- javascript/word-count/word_count_test.spec.js
+++ javascript/word-count/word_count_test.spec.js
@@ -10,4 +10,13 @@ describe('word_count_test.spec', function() {
             return wordCount();
         }).toThrow();
     });
+
+    it('should throw if input parameter is not string', function() {
+        expect(function () {
+            return wordCount(12343);
+        }).toThrow();
+        expect(function () {
+            return wordCount("jdsldd");
+        }).not.toThrow();
+    });
 });
