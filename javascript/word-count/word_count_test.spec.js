f12c49f274712acaa16f543c832fd040ad4319b6
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-09-07 18:54:28 +0300

    Add test to throwing

--- javascript/word-count/word_count_test.spec.js
+++ javascript/word-count/word_count_test.spec.js
@@ -4,4 +4,10 @@ describe('word_count_test.spec', function() {
     it('should export word count function', function() {
         expect(wordCount).toBeDefined();
     });
+
+    it('should throw if no input parameters passed', function() {
+        expect(function () {
+            return wordCount();
+        }).toThrow();
+    });
 });
