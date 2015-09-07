f25c29d2c93d5a9d8b13df118e264b2d81be9aea
Maksim Ryzhikov <rv.maksim@gmail.com>
2015-09-07 19:54:14 +0300

    pass test

--- javascript/word-count/word_count.js
+++ javascript/word-count/word_count.js
@@ -5,8 +5,10 @@ module.exports = function wordCount(sentence) {
         throw "Parameter should be a string";
     }
 
-    sentence.trim().replace(/\s+/ig, ' ').split(' ').forEach(function(word) {
-        word = word.replace(/[^a-z\-^0-9]+/ig, '');
+    sentence.trim().replace(/\s+/ig, ' ').split(' ').filter(function(word) {
+        return /[A-zА-я-]+/.test(word);
+    }).forEach(function(word) {
+        word = word.replace(/[^a-zа-я\-^0-9]+/ig, '');
         if (!wordsMap[word]) {
             wordsMap[word] = 1;
         } else {
